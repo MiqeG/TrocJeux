43f8f9e61e6232b8822abf0903d66a5809b96f04
MiqeG <mghilescu@protonmail.com>
2019-02-19 06:42:50 +0100

    done file input

--- public/sitescripts/fileinput.js
+++ public/sitescripts/fileinput.js
@@ -1,34 +1,48 @@
 function readURL(input) {
     if (input.files && input.files[0]) {
-  
-      var reader = new FileReader();
-  
-      reader.onload = function(e) {
-        $('.image-upload-wrap').hide();
-    
-        $('.file-upload-image').attr('src', e.target.result);
-        $('.file-upload-content').show();
-  
-        $('.image-title').html(input.files.length+' Fichiers');
-      };
-  
-      reader.readAsDataURL(input.files[0]);
+        if (input.files.length > 3) {
+            alert('Maximum 3 images.')
+            return
+        }
+       
+        $('.file-upload-image').hide()
+        $('.image-title').html(input.files.length);
       
-  
+        for (let index = 0; index < input.files.length ; index++) {
+
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+
+
+                $('.file-upload-image' + '.' + index).attr('src', e.target.result);
+               
+               
+
+              
+            };
+            for (let index = 0; index < input.files.length ; index++) {
+                $('.file-upload-image'+'.'+index).show();
+            }
+            $('.image-upload-wrap').hide();
+            $('.file-upload-content').show();
+          
+            reader.readAsDataURL(input.files[index])
+        }
+      
     } else {
-      removeUpload();
+        removeUpload();
     }
-  }
-  
-  function removeUpload() {
+}
+
+function removeUpload() {
     $('.file-upload-input').replaceWith($('.file-upload-input').clone());
     $('.file-upload-content').hide();
     $('.image-upload-wrap').show();
-  }
-  $('.image-upload-wrap').bind('dragover', function () {
-          $('.image-upload-wrap').addClass('image-dropping');
-      });
-      $('.image-upload-wrap').bind('dragleave', function () {
-          $('.image-upload-wrap').removeClass('image-dropping');
-  });
-  
+}
+$('.image-upload-wrap').bind('dragover', function () {
+    $('.image-upload-wrap').addClass('image-dropping');
+});
+$('.image-upload-wrap').bind('dragleave', function () {
+    $('.image-upload-wrap').removeClass('image-dropping');
+});
